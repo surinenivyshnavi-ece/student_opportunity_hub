62cec2b4e0b19f70142ef9c3875a4ad1723ba896
surinenivyshnavi-ece <surinenivyshnavi2006@gmail.com>
2026-06-15 17:02:47 +0530

    final update with firebase and UI fixes

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'internships_page.dart';
 import 'hackathons_page.dart';
 import 'team formation_page.dart';
 import 'profile_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
